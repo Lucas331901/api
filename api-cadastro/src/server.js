6df8b4cbcb7c0e3b017436a781b99385be44435e
Lucas331901 <127911431+Lucas331901@users.noreply.github.com>
2023-11-30 23:43:59 -0300

    sua mensagem

--- api-cadastro/src/server.js
+++ api-cadastro/src/server.js
@@ -15,6 +15,10 @@ app.use(express.json());
 app.use("/auth", AuthController);
 app.use("/admin", authenticateMiddleware, AdminController);
 
+app.get('/', (req, res) => {
+  res.status(200).json({ mensagem: 'Bem-vindo à API!' });
+});
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
