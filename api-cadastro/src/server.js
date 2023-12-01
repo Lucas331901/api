b794a42a3b3538b186a927603366be89d0734f16
Lucas331901 <127911431+Lucas331901@users.noreply.github.com>
2023-11-30 21:47:17 -0300

    sua mensagem

--- api-cadastro/src/server.js
+++ api-cadastro/src/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const  router = express.Router();
 
@@ -7,13 +8,14 @@ const AdminController = require("./controllers/AdminController");
 const authenticateMiddleware = require("./middlewares/authenticate");
 
 const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 
 app.use(express.json());
 
 app.use("/auth", AuthController);
 app.use("/admin", authenticateMiddleware, AdminController);
 
-app.listen(3001, ()=>{
-    console.log('Server is running');
-});
-//
